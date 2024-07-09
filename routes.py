8eea1894ce942104888774ae9201bac8b53b0db0
Cristian <cdanni91@gmail.com>
2024-07-09 18:11:56 -0300

    9-7-24

---
+++ routes.py
@@ -0,0 +1,11 @@
+from flask import request, jsonify
+from app import app, db
+from models import User
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    data = request.get_json()
+    new_user = User(first_name=data['first_name'], last_name=data['last_name'], age=data['age'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'User added successfully'}), 201
