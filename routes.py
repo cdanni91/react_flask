d2cb437d6f1eaa4d4782f611e7244f344f9f2c4b
Cristian <138404356+cdanni91@users.noreply.github.com>
2024-07-10 08:57:34 -0300

    Update routes.py

--- routes.py
+++ routes.py
@@ -4,10 +4,10 @@ from models import User
 
 @app.route('/add_user', methods=['POST'])
 def add_user():
-    data = request.get_json()
+    data = request.get_json()  # Obtiene los datos en formato JSON de la solicitud
     new_user = User(first_name=data['first_name'], 
                     last_name=data['last_name'],
-                      age=data['age'])
-    db.session.add(new_user)
-    db.session.commit()
-    return jsonify({'message': 'User added successfully'}), 201
+                    age=data['age'])  # Crea una nueva instancia del modelo User
+    db.session.add(new_user)  # Añade el nuevo usuario a la sesión de la base de datos
+    db.session.commit()  # Guarda (confirma) los cambios en la base de datos
+    return jsonify({'message': 'User added successfully'}), 201  # Retorna una respuesta JSON
